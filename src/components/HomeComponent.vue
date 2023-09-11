6575ee7030ba2005f1d8a6c08ce1c5d174293a85
Darynazar <heydari.std00@gmail.com>
2023-09-11 15:35:11 +0330

    first commit

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="">
+      <h1>{{name}}</h1>
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    data(){
+        return{
+            name: 'darynazar.com'
+        }
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
